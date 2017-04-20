23f6f4bc96633f3ad81f880df062c8cae8cffd2e
Massimo Perini <perinimassimo1995@gmail.com>
2017-04-20 03:18:04 +0200

    grunt

---
+++ website/Gruntfile.js
@@ -0,0 +1,115 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // Copy web assets from bower_components to more convenient directories.
+        copy: {
+            main: {
+                files: [
+                    // Vendor scripts.
+                    {
+                        expand: true,
+                        cwd: 'bower_components/bootstrap-sass/assets/javascripts/',
+                        src: ['**/*.js'],
+                        dest: 'public/javascripts/bootstrap/'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'bower_components/jquery/dist/',
+                        src: ['**/*.js', '**/*.map'],
+                     //	src: ['jquery.min.js', 'jquery.js', 'jquery.slim.js'],
+                        dest: 'public/javascripts/jQuery/'
+                    },
+
+                    // Fonts.
+                    {
+                        expand: true,
+                        filter: 'isFile',
+                        flatten: true,
+                        cwd: 'bower_components/',
+                        src: ['bootstrap-sass/assets/fonts/**'],
+                        dest: 'public/fonts/'
+                    }
+                ]
+            },
+        },
+
+        // Compile SASS files into minified CSS.
+        sass: {
+            options: {
+                includePaths: ['bower_components/bootstrap-sass/assets/stylesheets']
+            },
+            release: {
+                options: {
+                    outputStyle: 'compressed'
+                },
+                files: {
+               //     'css/app.css': 'scss/app.scss'
+               		'public/stylesheets/app.css': 'custom/scss/app.scss'
+                }
+            },
+            debug: {
+                options: {
+                    outputStyle: 'expanded'
+                },
+                files: {
+               		'public/stylesheets/app.css': 'custom/scss/app.scss'
+                }
+            }
+        },
+        
+        uglify: {
+    		debug: {
+      			options: {
+        			beautify: true,
+        			mangle: false
+      			},
+      			files: [{
+        			expand: true,
+        			cwd: 'custom/js',
+        			src: '**/*.js',
+        			dest: 'public/javascripts'
+      			}]
+    		},
+    		release: {
+      			files: [{
+        			expand: true,
+        			cwd: 'custom/js',
+        			src: '**/*.js',
+        			dest: 'public/javascripts'
+      			}]
+    		}
+  		},
+        // Watch these files and notify of changes.
+        watch: {
+            grunt: { files: ['Gruntfile.js'] },
+
+            sass: {
+                files: [
+                    'custom/scss/**/*.scss'
+                ],
+                tasks: ['sass:debug']
+            },
+            uglify: {
+                files: [
+                    'custom/js/**/*.js'
+                ],
+                tasks: ['uglify:debug']
+            }
+        }
+    });
+
+    // Load externally defined tasks. 
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Establish tasks we can run from the terminal.
+   // grunt.registerTask('build', ['sass', 'copy']);
+ //   grunt.registerTask('build', ['copy', 'sass', 'uglify']);
+    grunt.registerTask('debug', ['copy', 'sass:debug', 'uglify:debug']);
+    grunt.registerTask('release', ['copy', 'sass:release', 'uglify:release']);
+ //   grunt.registerTask('default', ['build', 'watch']);
+    //grunt build:debug oppure grunt build:release
+}
