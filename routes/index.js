54c52e66d7c05e0d70a2f8766362838538c26537
nicomon24 <nico.montali24@gmail.com>
2017-05-08 12:00:35 +0200

    Added index page, basic nodejs structure et altera

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'CPM clinic' });
+});
+
+router.get('/who-we-are', function(req, res, next){
+  res.render('who-we-are/who-we-are');
+});
+
+router.get('/who-we-are/statistics', function(req, res, next){
+  res.render('who-we-are/statistics');
+})
+
+module.exports = router;
