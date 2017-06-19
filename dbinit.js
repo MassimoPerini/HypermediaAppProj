7e0004e5af77af4ffcd62b09161fe2f3dac3e289
nicomon24 <nico.montali24@gmail.com>
2017-06-19 16:20:15 +0200

    dbinit modified to read data sequentially (foreign key error)

--- dbinit.js
+++ dbinit.js
@@ -8,15 +8,25 @@
 var models = require('./models');
 var debug = require('debug')('model');
 
+/*
+  TABLES to fetch from files are listed here, in order
+*/
+var tablesToFetch = ['locations', 'locations_timetables' 'areas', 'doctors', 'service', 'users', 'infos']
+
+async function executeSequentially(promises) {
+    for (const promise of promises) {
+        await promise;
+    }
+    return true;
+}
+
 debug("Updating schema...");
 return models.sequelize.sync({ force : true })
 .then(function(err){
   debug("DB schema is up-to-date");
   debug("Loading data...");
   var promises = [];
-  Object.keys(models)
-  .filter((key) => { return (key != 'sequelize' && key != 'Sequelize')})
-  .forEach((table) => {
+  tablesToFetch.forEach((table) => {
     try {
       var datas = require('./data/' + table + '.json');
       promises.push(models[table].bulkCreate(datas,{
@@ -26,10 +36,9 @@ return models.sequelize.sync({ force : true })
       debug('Did not find data for ' + table);
     }
   });
-  return Promise.all(promises);
+  return executeSequentially(promises);
 }).then(function(results){
   debug('Data loaded.');
-  //debug('Loading associations...'); TODO
   debug('DB ready, buckle up!');
   process.exit(0);
 }).catch(function(err){
