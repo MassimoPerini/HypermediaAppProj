5455b202c5f7efc5ef9a3fb737331c4bb9f8ac32
Andrea Corsini <andrea.corsini@outlook.com>
2017-06-24 10:53:39 +0200

    who we are, doctor table

--- routes/who-we-are.js
+++ routes/who-we-are.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/who-we-are', function(req, res, next){
-  res.render('who-we-are/who-we-are', { title: 'stacippa'});
+  res.render('who-we-are/who-we-are', { title: 'Who we are'});
 });
 
 router.get('/who-we-are/statistics', function(req, res, next){
