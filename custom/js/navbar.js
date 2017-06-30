7a6091a836255105ae37196353a81d338f1cf4c0
Massimo Perini <perinimassimo1995@gmail.com>
2017-06-30 21:25:19 +0200

    Footer fix with form

--- custom/js/navbar.js
+++ custom/js/navbar.js
@@ -1,23 +1,14 @@
-/**
- * Created by massimo on 30/06/17.
- */
-
 $(document).ready(function($) {
-    $(window).on('scroll', function() {
-        //ADD .TIGHT
-        var headerOuter = ($('header').length) ? $('header').outerHeight() : 0;
-        if ($(window).scrollTop() + $(window).height() > $('#content').outerHeight() + headerOuter) {
-            $('body').addClass('tight');
-        } else {
-            $('body').removeClass('tight');
-        }
-    });
-
-    //BACK TO PRESENTATION MODE
-    $("html").on("click", "body.tight #content", function() {
-        $('html, body').animate({
-            scrollTop: $('#content').outerHeight() - $(window).height()
+    console.log("isInside ");
+    var isFormInside = $('form') ? 1 :  0;
+    isFormInside*=150;
+    console.log("isInside "+isFormInside);
+    $(window).on("scroll", function() {
+        var headerOuter = $("header").length ? $("header").outerHeight() : 0;
+        $(window).scrollTop() + $(window).height() > ($("#content").outerHeight() + headerOuter + isFormInside) ? $("body").addClass("tight") : $("body").removeClass("tight");
+    }), $("html").on("click", "body.tight #content", function() {
+        $("html, body").animate({
+            scrollTop: $("#content").outerHeight() - $(window).height()
         }, 500);
     });
-
 });
--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -1,7 +1,9 @@
 $(document).ready(function($) {
-    $(window).on("scroll", function() {
+    console.log("isInside ");
+    var isFormInside = $("form") ? 1 : 0;
+    isFormInside *= 150, console.log("isInside " + isFormInside), $(window).on("scroll", function() {
         var headerOuter = $("header").length ? $("header").outerHeight() : 0;
-        $(window).scrollTop() + $(window).height() > $("#content").outerHeight() + headerOuter ? $("body").addClass("tight") : $("body").removeClass("tight");
+        $(window).scrollTop() + $(window).height() > $("#content").outerHeight() + headerOuter + isFormInside ? $("body").addClass("tight") : $("body").removeClass("tight");
     }), $("html").on("click", "body.tight #content", function() {
         $("html, body").animate({
             scrollTop: $("#content").outerHeight() - $(window).height()
