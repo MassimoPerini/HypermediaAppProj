b56b76e82ac694a22d66a3b30d169e441a76370b
nicomon24 <nico.montali24@gmail.com>
2017-07-02 14:09:41 +0200

    Added sidebar for mobile

--- custom/js/navbar.js
+++ custom/js/navbar.js
@@ -1,4 +1,5 @@
 $(document).ready(function($) {
+    /* STICKY FOOTER */
     var isFormInside = 1;//$('form').length > 0 ? 1 :  0;
     //console.log($('form'));
     isFormInside*=200;
@@ -10,4 +11,35 @@ $(document).ready(function($) {
             scrollTop: $("#content").outerHeight() - $(window).height()
         }, 500);
     });
+    /* SIDEBAR NAV */
+    updateNavbar();
 });
+
+$(window).on("resize", function () {
+  updateNavbar();
+});
+
+function updateNavbar(){
+  if ($(window).width() >= 992 && $('.navbar-toggle').is(':hidden')) {
+    $('#sidebar').width('0');
+    $('#sidebar-bg').width('0');
+    $('.brand').removeClass('pull-right');
+    $('.navbar-toggle').removeClass('pull-left');
+  }else{
+    $('#sidebar').width('0');
+    $('#sidebar-bg').width('0');
+    $('.brand').addClass('pull-right');
+    $('.navbar-toggle').addClass('pull-left');
+  }
+}
+
+function toggleNav(){
+  if ($(window).width() >= 992 && $('.navbar-toggle').is(':hidden')) return;
+  if ($('#sidebar').width() <= 0){
+    $('#sidebar').width('70%');
+    $('#sidebar-bg').width('100%');
+  }else{
+    $('#sidebar').width('0');
+    $('#sidebar-bg').width('0');
+  }
+}
--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -1,3 +1,14 @@
+function updateNavbar() {
+    $(window).width() >= 992 && $(".navbar-toggle").is(":hidden") ? ($("#sidebar").width("0"), 
+    $("#sidebar-bg").width("0"), $(".brand").removeClass("pull-right"), $(".navbar-toggle").removeClass("pull-left")) : ($("#sidebar").width("0"), 
+    $("#sidebar-bg").width("0"), $(".brand").addClass("pull-right"), $(".navbar-toggle").addClass("pull-left"));
+}
+
+function toggleNav() {
+    $(window).width() >= 992 && $(".navbar-toggle").is(":hidden") || ($("#sidebar").width() <= 0 ? ($("#sidebar").width("70%"), 
+    $("#sidebar-bg").width("100%")) : ($("#sidebar").width("0"), $("#sidebar-bg").width("0")));
+}
+
 $(document).ready(function($) {
     var isFormInside = 1;
     isFormInside *= 200, $(window).on("scroll", function() {
@@ -7,5 +18,7 @@ $(document).ready(function($) {
         $("html, body").animate({
             scrollTop: $("#content").outerHeight() - $(window).height()
         }, 500);
-    });
+    }), updateNavbar();
+}), $(window).on("resize", function() {
+    updateNavbar();
 });
